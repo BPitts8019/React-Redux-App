3aff8d4d43289d9629258eb827d6c6984ef770b5
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-25 13:21:22 -0700

    Setup Redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import {createStore, applyMiddleware, combineReducers} from "redux";
-import thunk from "redux-thunk";
+import thunk from "redux-thunk"; //allows our action creators to be HOFs
 //import reducers
 
 const rootReducer = combineReducers({
